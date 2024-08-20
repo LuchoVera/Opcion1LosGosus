b6d0efa6e4c4f4b3ae75dd1f0b83a8f343fa6a25
JosuePrado <prado69508698@gmail.com>
2024-08-19 22:17:33 -0400

    fix: end line

--- src/Models/Reserve.cs
+++ src/Models/Reserve.cs
@@ -3,5 +3,4 @@ public class Reserve(Patron reservedBy, Book reservedBook)
     public Patron ReservedBy { get; private set; } = reservedBy;
     public Book ReservedBook { get; private set; } = reservedBook;
 
-    
 }
