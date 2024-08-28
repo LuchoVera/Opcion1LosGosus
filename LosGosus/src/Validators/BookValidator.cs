e9ada0305e2dfcf7a13b974cbaea4bc894de93d2
Alex Paca <fujorita772@gmail.com>
2024-08-28 16:47:40 -0400

    refactor: deleting validations with same body

--- LosGosus/src/Validators/BookValidator.cs
+++ LosGosus/src/Validators/BookValidator.cs
@@ -2,19 +2,9 @@ public class BookValidator : IValidator<Book>
 {
     private const int maxCharactersLength = 100;
     private const int minCharactersLength = 1;
-    protected static bool ValidateTitle(string title) {
-        return ValidateNullOrEmpty(title);
-    }
 
-    protected static bool ValidateAuthor(string author) {
-        return ValidateNullOrEmpty(author);
-    }
     protected static bool ValidateISBN(string isbn) {
         return !string.IsNullOrEmpty(isbn) && isbn.StartsWith("isbn-") && isbn.Length == 15 && isbn.Substring(5).All(char.IsDigit);
-    }
-
-    protected static bool ValidateGenre(string genre) {
-        return ValidateNullOrEmpty(genre);
     }
 
     protected static bool ValidatePublicationYear(int publicationYear) {
@@ -30,10 +20,10 @@ public class BookValidator : IValidator<Book>
     public Dictionary<string, bool> GetValidationResults(Book book) {
         return new Dictionary<string, bool>
         {
-            { "Title", ValidateTitle(book.Title) },
-            { "Author", ValidateAuthor(book.Author) },
+            { "Title", ValidateNullOrEmpty(book.Title) },
+            { "Author", ValidateNullOrEmpty(book.Author) },
             { "ISBN", ValidateISBN(book.ISBN) },
-            { "Genre", ValidateGenre(book.Genre) },
+            { "Genre", ValidateNullOrEmpty(book.Genre) },
             { "PublicationYear", ValidatePublicationYear(book.PublicationYear) }
         };
     }
@@ -42,5 +32,4 @@ public class BookValidator : IValidator<Book>
         var validationResults = GetValidationResults(book);
         return validationResults.All(result => result.Value);
     }
-
 }
